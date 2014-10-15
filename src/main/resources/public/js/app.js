16bec2e21cc438a5be51a115a0b1143699aae486
pbonanno <ormappingd5>
2014-10-15 23:44:16 +0200

    Aggiornamento del progetto

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -4,19 +4,56 @@ angular.module('app', ['ngRoute', 'ngResource', 'angular-hal'])
 		$routeProvider
             .when('/', {
 			     templateUrl: 'list.html'
-		    })
-            .when('/detail', {
+		    }).when('/detail', {
 			     templateUrl: 'detail.html'
-		    }).otherwise({
+		    })
+		    .otherwise({
 			     redirectTo: '/'
 		    });
 })
 
 
-.controller('DetailCtrl', function($scope, $resource, halClient) {
-    /*
-	var a = halClient.$get('api').then( function( api ) {
+
+.controller('ListCtrl', function($scope, $resource, $location, halClient, $window) {
+    var a = halClient.$get('api').then( function( api ) {
         return api.$get('fattura').then(function (fattura) {
+            return fattura.$get('fattura').then(function (fatture) {
+                $scope.fatture = fatture;
+            });
+
+        });
+    })
+    ;
+   
+//    $scope.detailPage = function(idx) {
+//        var id = $scope.fatture[idx].id;
+//        console.log(id);
+//        $location.path("/detail/"+id);
+//    }
+    
+})
+
+.controller('DetailCtrl', function($scope, $resource, halClient, $routeParams) {
+//	var id = $routeParams.id;
+
+//	var fattura = halClient.$get('fattura', {'id': id}).then(function(testata) {
+//		$scope.testata = testata;
+//		
+//		$scope.testata.$get('righe').then(function(righe) {
+//			return righe.$get('rigaFattura').then(function(righe) {
+//				$scope.righe = righe;
+//			});
+//		});
+//		
+//	});
+	var href = null;
+	
+	halClient.$get("api/rigaFattura").then(function(riga) {
+		href = riga.$href('self');
+	});
+	
+	var a = halClient.$get('api').then( function( api ) {
+        api.$get('fattura').then(function (fattura) {
         	return fattura.$get('fattura').then(function (fatture) {
     			$scope.testata = fatture[0];
     			console.log($scope.testata);
@@ -32,29 +69,24 @@ angular.module('app', ['ngRoute', 'ngResource', 'angular-hal'])
         });
     })
     ;
-	*/
+	
+	$scope.aggiungiRiga = function() {
+		$scope.righe.push({
+		      "riga" : 0,
+		      "descrizione" : "Riga 0",
+		      "quantita" : 0,
+		      "prezzoUnitario" : 10.00,
+		      "iva" : 22.00
+		      });
+		
+		halClient.createResource(href, {} , {
+		      "riga" : 0,
+		      "descrizione" : "Riga 0",
+		      "quantita" : 0,
+		      "prezzoUnitario" : 10.00,
+		      "iva" : 22.00
+		      })
+	};
 
 })
-
-.controller('ListCtrl', function($scope, $resource, $location, halClient) {
-    var a = halClient.$get('api').then( function( api ) {
-        return api.$get('fattura').then(function (fattura) {
-            return fattura.$get('fattura').then(function (fatture) {
-                $scope.fatture = fatture;
-            });
-
-        });
-    })
-    ;
-   
-    $scope.detail = function(idx) {
-        console.log($scope.fatture[idx]);
-
-        var href = $scope.fatture[idx].$href('self');
-        $location.path("/detail");
-
-        // halClient.$get($scope.fatture[idx].$href('self')).then(function(f) {
-        //     console.log("dettaglio: "); console.log(f);
-        // });
-    }
-});
+;
